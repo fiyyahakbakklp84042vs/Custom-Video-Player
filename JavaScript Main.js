ea398d7e5e9d9c633403e3ecd66b42eeef2a78b9
fiyyahakbakklp84042vs <49891756+fiyyahakbakklp84042vs@users.noreply.github.com>
2020-03-29 19:58:56 +0800

    Update JavaScript Main.js
    Update Video for previous video and next video in the video page

--- JavaScript Main.js
+++ JavaScript Main.js
@@ -1,72 +1,79 @@
-var vid = document.getElementById("myVideo");
-
+var vid = document.getElementById('myVideo');
 var fillBar = document.getElementById("fill");
-
 var currentTime = document.getElementById("currentTime");
-
 var volumeSlider = document.getElementById("volume");
-
-function playOrPause(){
-    
-    if(vid.paused){
-        vid.play();
-        $("#playBtn").attr("src","Pause.png");
-    }
-    else{
-        vid.pause();
-        $("#playBtn").attr("src","Play.png");
-    }
+var s = 1;
+function playOrPause() {
+	if (vid.paused) {
+		vid.play();
+		$("#playBtn").attr("src","Custom-Video-Player-master/pause.png");
+	} else {
+		vid.pause();
+		$("#playBtn").attr("src","Custom-Video-Player-master/play.png");
+	}
 }
-
-vid.addEventListener('timeupdate',function(){
-    
-    var position = vid.currentTime / vid.duration;
-        
-    fillBar.style.width = position * 100 +'%';
-    
-    convertTime(Math.round(vid.currentTime));  //convert decimal no into intiger
-    
-    if(vid.ended){
-        $("#playBtn").attr("src","Play.png");
-    }
+vid.addEventListener('timeupdate', function(){
+	var position = vid.currentTime / vid.duration;
+	fillBar.style.width = position * 100 + '%';
+	converTime(Math.round(vid.currentTime));
+	if (vid.ended) {
+		next();
+	}
 });
-
-function convertTime(seconds)
-        {
-            var min = Math.floor(seconds / 60);
-            var sec = seconds % 60;
-            
-            min = (min < 10) ? "0" + min : min;
-            sec = (sec < 10) ? "0" + sec : sec;
-            currentTime.textContent = min + ":" + sec;
-            
-            totalTime(Math.round(vid.duration));
-        }
-        
-        function totalTime(seconds)
-        {
-            var min = Math.floor(seconds / 60);
-            var sec = seconds % 60;
-            
-            min = (min < 10) ? "0" + min : min;
-            sec = (sec < 10) ? "0" + sec : sec;
-            currentTime.textContent += " / " + min + ":" + sec;
-        }
-
-
-function changeVolume(){
-    
-    vid.volume = volumeSlider.value;
-    
-    if(volumeSlider.value == 0){
-        
-        $("#speaker").attr("src","Mute.png");
-    }
-    else{
-        $("#speaker").attr("src","Speaker.png");
-    }
+function converTime(seconds){
+	var min = Math.floor(seconds / 60);
+	var sec = seconds % 60;
+	min = (min < 10) ? "0" + min : min;
+	sec = (sec < 10) ? "0" + sec : sec;
+	currentTime.textContent = min + ":" + sec;
+	totalTime(Math.round(vid.duration));
 }
-
-
-
-
+function totalTime(seconds){
+	var min = Math.floor(seconds / 60);
+	var sec = seconds % 60;
+	min = (min < 10) ? "0" + min : min;
+	sec = (sec < 10) ? "0" + sec : sec;
+	currentTime.textContent += " / " + min + ":" + sec; 
+}
+function changeVolume(){
+	vid.volume = volumeSlider.value;
+	if (volumeSlider.value == 0) {
+		$("#speaker").attr("src","Custom-Video-Player-master/Mute.png");
+	} else {
+		$("#speaker").attr("src","Custom-Video-Player-master/Speaker.png")
+	}
+}
+function next(){
+	s++;
+	if (s == 4) {
+		s = 1;
+	};
+	charge();
+}
+function previous(){
+	s--;
+	if (s == 0) {
+		s = 3;
+	};
+	charge();
+}
+function charge(){
+	title = document.getElementById("title");
+	switch(s){
+		case 1:
+		vid.src = "9 WAR OF UNITY.mp4";
+		vid.poster = "vlcsnap-2020-03-17-17h52m24s394.png";
+		playOrPause();
+		break;
+		case 2:
+		vid.src = "KICKO & SUPER SPEEDO VS CAR QUEEN FULL EPISODE IN HINDI.mp4";
+		vid.poster = "vlcsnap-2020-03-17-17h52m24s394.png";
+		playOrPause();
+		break;
+		case 3:
+		vid.src = "KICKO & SUPER SPEEDO AND HAWAI TOLL NEW EPISODE IN HINDI.mp4";
+		vid.poster = "vlcsnap-2020-03-17-17h52m24s394.png";
+		playOrPause();
+		break;
+	}
+}
